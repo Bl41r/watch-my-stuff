bf4b74d8dd319268a2f8e3097db251c37f6b30c0
bl41r <dbsmith.dbs83@gmail.com>
2017-06-20 18:44:33 -0700

    reqs, script, ibm file added

---
+++ watcher.py
@@ -0,0 +1,58 @@
+"""Watch my stuff.
+
+David Smith, 2017
+"""
+import datetime as dt
+import time
+import os
+import cv2
+
+CASCADE_MODEL = 'haarcascade_frontalface_default.xml'
+FACE_CASCADE = cv2.CascadeClassifier(CASCADE_MODEL)
+
+
+def main(debug=True):
+    """Watch over my desk with vigilence."""
+    print('Initializing...')
+    video_capture = cv2.VideoCapture(0)
+    video_capture.set(3, 640)
+    video_capture.set(4, 480)
+    x = 0
+    time.sleep(2)
+
+    while True:
+        print('Now watching!')
+        ret, frame = video_capture.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = FACE_CASCADE.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+        )
+
+        if debug:
+            for (x, y, w, h) in faces:
+                cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        if len(faces) == 1:
+            time.sleep(1)
+            cv2.imwrite(
+                os.path.join('img', str(dt.datetime.now()) + '.png'),
+                gray
+            )
+
+        if debug:
+            cv2.imshow('frame', gray)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        if debug:
+            cv2.waitKey(1)
+            cv2.destroyAllWindows()
+            for i in range(5):
+                cv2.waitKey(1)
+
+if __name__ == '__main__':
+    main(debug=True)
